2ee817c4ee569b40a878a7a2d480da786ae83021
AuHop <austin.hoppe7@live.com>
2013-09-29 13:14:15 -0500

    Create includes_stringah1.h

---
+++ stringah1/includes_stringah1.h
@@ -0,0 +1,20 @@
+/* Austin Hoppe
+ * 29 September 2013
+ * includes_stringah1.h */
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifndef __INCLUDES_STRINGAH1_H__
+#define __INCLUDES_STRINGAH1_H__
+
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+#endif /* __INCLUDES_STRINGAH1_H__ */
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
